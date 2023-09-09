fbe12ed4c6d858e3c1cbd46a52f85a23bc365f6d
Priyanka-Das-Dipa <priyankadipa9102@gmail.com>
2023-09-10 01:49:14 +0600

    second commit

--- src/Component/Country/Country.jsx
+++ src/Component/Country/Country.jsx
@@ -1,11 +1,13 @@
 import './Country.css';
 const Country = ({country}) => {
     console.log(country)
-    const {name, flags} = country;
+    const {name, flags, population, area} = country;
     return (
         <div className='country'>
             <h3>Name:{name?.common} </h3>
             <img src={flags.png} alt="" />
+            <p>Population Of the {name?.common} is : {population} </p>
+            <p>Area Of the {name?.common} is : {area} </p>
             {/* <img src={flags.svg} alt="" /> */}
         </div>
     );
